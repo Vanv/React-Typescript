7d78cf37def2549997f37da0fb43324dbaf7531a
Van <vancreative@gmail.com>
2020-02-14 13:43:46 +1300

    interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Message from './Message'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component<any> {
+
+
+  render() {
+
+    // let firstValue: string = "Mike";
+    //let firstValue: number = 66;
+    // let firstValue: boolean = true;
+    // let firstValue: number[] = [2, 3, 66];
+    //let firstValue: Array<string> = ["2", "66", "Van" ];
+
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {/* <p>The value {firstValue} if of {typeof firstValue} type</p> */}
+          <Message name="Van" message="This is a quick message from app"/>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
